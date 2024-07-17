3719e069115ae11546d49a8759d22ee63035e376
MJebelli <s.m.jebelli@gmail.com>
2024-07-17 17:39:22 +0330

    first correct answer

--- 3.LognestSubstringWithoutRepeating/LongestSubstring/LongestSubstring/Program.cs
+++ 3.LognestSubstringWithoutRepeating/LongestSubstring/LongestSubstring/Program.cs
@@ -1,13 +1,30 @@
-﻿namespace LongestSubstring
+﻿using System.Runtime.InteropServices;
+
+namespace LongestSubstring
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string str = "abcabcbb";
+            Console.WriteLine(str);
+            LengthOfLongestSubstring(str);
+
+
+            str = "bbbbb";
+            Console.WriteLine(str);
+            LengthOfLongestSubstring(str);
+
+
+            str = "pwwkew";
+            Console.WriteLine(str);
+            LengthOfLongestSubstring(str);
         }
 
         /*
+         Given a string s, find the length of the longest substring without repeating characters.
+         *
+         *
          Example 1:
 
         Input: s = "abcabcbb"
@@ -25,9 +42,35 @@
         Explanation: The answer is "wke", with the length of 3.
         Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
          */
-        public int LengthOfLongestSubstring(string s)
+        static int LengthOfLongestSubstring(string s)
         {
+            int maxLen = 0;
 
+            var sp = s.AsSpan();
+
+            for (int i = 1; i <= sp.Length; i++)
+            {
+                for (int j = 0; j <= sp.Length - i; j++)
+                {
+                    char[] chars = new char[i];                    
+                    for (int k = 0; k < i; k++)
+                    {
+                        chars[k] = sp[j+k];
+                    }
+                    //check if array has duplicate
+                    int unqLength = chars.Distinct().ToArray().Length;
+                    //Console.WriteLine($"i:{i}\tj:{j}\tchars: {new string(chars)} unqLength : {unqLength}" );
+
+                    if (chars.Distinct().ToArray().Length == chars.Length && maxLen<chars.Length)
+                    {
+                        maxLen = chars.Length;
+                        Console.WriteLine($"max length updated to : {maxLen} for: {new string(chars)}");
+                    }
+                }
+
+
+            }
+            return maxLen;
         }
     }
 }
