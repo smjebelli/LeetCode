bc0f894f09e6ec6f3c59e7a5ac18f3d1184682bc
MJebelli <s.m.jebelli@gmail.com>
2024-07-26 20:11:07 +0330

    LinkList Cycle accepted

---
+++ 141.LinkedListCycle/LinkedListCycle/LinkedListCycle/ListNode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListCycle
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x)
+        {
+            val = x;
+            next = null;
+        }
+    }
+}
